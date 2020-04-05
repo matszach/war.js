3b18e792cdb6346bcbc181e13d662ecf16334c45
matszach <lkaszubowski@gmail.com>
2020-04-06 00:40:50 +0200

    +

--- js/mock.js
+++ js/mock.js
@@ -15,7 +15,7 @@ function getRandomTeam(color, nofMembers, rootX, rootY) {
     Gmt.iter1D(nofMembers, i => {
         let c = Gmt.choice(UNIT_TYPES);
         let w = new c();
-        w.placeAt(rootX + Gmt.randFloat(-200, 200), rootY + Gmt.randFloat(-400, 400));
+        w.placeAt(rootX + Gmt.randFloat(-200, 200), rootY + Gmt.randFloat(-350, 350));
         w.turnTo(Gmt.rad(Gmt.randFloat(0, 2)));
         t.warriors.push(w);
     });
@@ -23,14 +23,7 @@ function getRandomTeam(color, nofMembers, rootX, rootY) {
 } 
 
 function getTeams() {
-    let nofTeams = Gmt.randInt(2, 2);
-    let nofMembers = parseInt(100/nofTeams);
-    let colors = [];
-    while(colors.length < nofTeams) {
-        let c = Gmt.choice(COLORS);
-        if(!colors.includes(c)){
-            colors.push(c);
-        }
-    }
-    return Gmt.constructArray(nofTeams, i => getRandomTeam(colors[i], nofMembers, Gmt.randFloat(200, 1400), 400));
+    let team1 = getRandomTeam(Gmt.choice(COLORS), 250, 250, 400);
+    let team2 = getRandomTeam(Gmt.choice(COLORS.filter(w => w.color !== team1.color)), 250, 1250, 400);
+    return [team1, team2];
 }
